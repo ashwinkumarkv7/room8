006fcd0a1a8390f49ba3c9005d4a56ce5fa87ff6
ashwinkumarkv7 <ashwinkumarkv7@gmail.com>
2025-08-10 21:55:03 +0530

    config api

--- client/src/apiConfig.js
+++ client/src/apiConfig.js
@@ -1,7 +1,7 @@
 // Change this:
-// const API_URL = 'https://room8-server.onrender.com';
+const API_URL = 'https://room8-server.onrender.com';
 
 // To this:
-const API_URL = 'http://localhost:5000';
+// const API_URL = 'http://localhost:5000';
 
 export default API_URL;
